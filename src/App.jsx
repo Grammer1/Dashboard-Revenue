32f877d6b088198656b7a3fca88a4d41a44ceb1b
Grammer1 <grammergame86@gmail.com>
2026-04-11 20:56:07 +0700

    get logout 30

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,23 @@ function App() {
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
-    const auth = localStorage.getItem("auth");
-    setIsAuth(!!auth);
-  }, []);
+  const interval = setInterval(() => {
+    const loginTime = localStorage.getItem("loginTime");
+
+    if (loginTime) {
+      const diff = Date.now() - parseInt(loginTime);
+      const THIRTY_MINUTES = 30 * 60 * 1000;
+
+      if (diff > THIRTY_MINUTES) {
+        localStorage.removeItem("auth");
+        localStorage.removeItem("loginTime");
+        setIsAuth(false);
+      }
+    }
+  }, 60000); // cek tiap 1 menit
+
+  return () => clearInterval(interval);
+}, []);
 
   if (!isAuth) {
     return <Login onLogin={() => setIsAuth(true)} />;
